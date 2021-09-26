f9e90a03968d7de0d3b115e8bff1e21f579a5fe6
Bruno Barbagelata <bruno140685@gmail.com>
2021-09-26 15:04:15 -0400

    working on backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
 
-console.log("working");
+app.get("/", (req, res) => {
+  console.log("hellohello");
+  res.json({ animal: "racoon" });
+});
+
+console.log("hello");
+
+app.listen(process.env.PORT || 5000);
