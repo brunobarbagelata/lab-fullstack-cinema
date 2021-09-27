5f0d902f5620f056edf3973db57dfda533baeb01
Bruno Barbagelata <bruno140685@gmail.com>
2021-09-27 09:01:33 -0400

    working on backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const Movie = require("./models/Movie");
+const cors = require("cors");
+
+mongoose
+  .connect(`mongodb://localhost:27017/movieDB`)
+  .then((self) =>
+    console.log(`Connected to the database: "${self.connection.name}"`)
+  );
+
+const app = express();
+app.use(cors()); //for cors
+app.use(express.json()); //for req.body
+
 console.log("its working");
+
+app.get("/", (req, res) => {
+  res.json({ name: "hello" });
+});
+
+app.listen(process.env.PORT || 5000);
